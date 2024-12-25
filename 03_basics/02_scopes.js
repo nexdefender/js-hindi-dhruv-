5468eb73439d86f8f8c574eac8f0d5f81563db03
Dhruv bansal <cybosecdhruv@gmail.com>
2024-12-25 13:33:57 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+// var c = 300
+let a = 40
+
+if(true)
+{
+    let a= 10
+const b= 20
+console.log("inneer: ",a);
+
+// var c=30
+
+}
+
+
+// only c will print throught this other two not come out of if because not return value was there
+
+
+// console.log(c);
+console.log(a);
+
+
+
