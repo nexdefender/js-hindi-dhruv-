74514665b6d98fbf3696abf75c065cba96af5a29
Dhruv bansal <cybosecdhruv@gmail.com>
2024-12-26 16:25:04 +0000

    WHILE,DOWHILE

---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+let i = 0 
+
+while (i<=10) {
+    console.log(`value of i is ${i}`);
+    i=i+2
+    
+}
+
+let myArray = ["ironman","spiderman","thor"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+let score = 5
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
+
+while (score<=11) {
+    console.log(`score is ${score}`);
+    score++;
+}
+
+// DIFFERENCE BTW WHILE OR DO WHILE IS //
+// WHILE LOOP: CONDITION IS FIRST
+// DO WHILE LOOP: CONDITION IN END
+// EX: GIVEN ABOVE
